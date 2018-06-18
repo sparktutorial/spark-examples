a44029602a2aca85f1d1e491ad6efce5d3b35485
sparktutorial <ash98778@gmail.com>
2018-06-18 09:04:21 +0530

    Maven Spark Program

---
+++ MyApplication/src/main/scala/WordCount.scala
@@ -0,0 +1,20 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by cloudwick on 6/18/2018.
+  */
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("WordCount")
+    val sc = new SparkContext(conf)
+
+    val input = args(0)
+    val output = args(1)
+
+    val inputData = sc.textFile(input, 3)
+    val finalResult = inputData.flatMap(x => x.split(",")).map(x => (x, 1)).reduceByKey((x, y) => x + y)
+    finalResult.collect.foreach(println)
+
+    finalResult.saveAsTextFile(output)
+  }
+}

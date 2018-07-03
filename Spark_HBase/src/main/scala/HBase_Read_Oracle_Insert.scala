4ac00925844f18d4d97ce4a28ad8e88f9a1a7018
sparktutorial <ash98778@gmail.com>
2018-07-03 15:54:03 +0530

    Modified the logic to get the status value from json

--- Spark_HBase/src/main/scala/HBase_Read_Oracle_Insert.scala
+++ Spark_HBase/src/main/scala/HBase_Read_Oracle_Insert.scala
@@ -1,3 +1,5 @@
+import java.sql.{DriverManager, SQLException}
+
 import net.liftweb.json.{DefaultFormats, JArray, parse}
 import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
 import org.apache.hadoop.hbase.client.{ConnectionFactory, ResultScanner, Scan, Table}
@@ -9,34 +11,55 @@ import org.apache.hadoop.hbase.util.Bytes
 object HBase_Read_Oracle_Insert {
   implicit val formats = DefaultFormats
   def main(args: Array[String]): Unit = {
+
+    if(args.length != 6) {
+      throw new RuntimeException("Invalid Number of Arguments...!!!")
+    }
+
+    val zookeeperList = args(0)
+    val zookeeperPort = args(1)
+    val hBaseNameSpace = args(2)
+    val oracleJdbcUrl = args(3)
+    val oracleUserName = args(4)
+    val oraclePassword = args(5)
+    //val batchSize = 100
+
+    Class.forName("oracle.jdbc.driver.OracleDriver")
+    val con = DriverManager.getConnection(oracleJdbcUrl,oracleUserName,oraclePassword)
+    val stmt = con.createStatement()
+
     val conf = HBaseConfiguration.create()
-    conf.set("hbase.zookeeper.quorum", "quickstart.cloudera")
-    conf.set("hbase.zookeeper.property.clientPort", "2181")
+    conf.set("hbase.zookeeper.quorum", s"$zookeeperList")
+    conf.set("hbase.zookeeper.property.clientPort", s"${zookeeperPort}")
 
     val conn = ConnectionFactory.createConnection(conf)
 
-    val table: Table = conn.getTable(TableName.valueOf("job_status"))
+    val table: Table = conn.getTable(TableName.valueOf(s"$hBaseNameSpace", "job_status"))
     val scan: Scan = new Scan()
     val scanner: ResultScanner = table.getScanner(scan)
 
     val iter = scanner.iterator()
+    var rowCount = 0L
+
     while (iter.hasNext) {
       val record = iter.next()
       val rowKey = Bytes.toString(record.getRow())
-      println("Row Key :: " + rowKey)
+      //println("Row Key :: " + rowKey)
       val failMsg = Bytes.toString(record.getValue(Bytes.toBytes("f"), Bytes.toBytes("fail_msg")))
-      println("f:fail_msg :: " + failMsg)
+      //println("f:fail_msg :: " + failMsg)
       val st = Bytes.toString(record.getValue(Bytes.toBytes("f"), Bytes.toBytes("st")))
-      println("f:st :: " + st)
+      //println("f:st :: " + st)
       val cfg = Bytes.toString(record.getValue(Bytes.toBytes("f"), Bytes.toBytes("cfg")))
-      println("f:cfg :: " + cfg)
+      //println("f:cfg :: " + cfg)
       val addTimeStamp = Bytes.toString(record.getValue(Bytes.toBytes("f"), Bytes.toBytes("add_ts")))
-      println("f:add_ts :: " + addTimeStamp)
+      //println("f:add_ts :: " + addTimeStamp)
       val doneTimeStamp = Bytes.toString(record.getValue(Bytes.toBytes("f"), Bytes.toBytes("done_ts")))
-      println("f:done_ts :: " + doneTimeStamp)
+      //println("f:done_ts :: " + doneTimeStamp)
 
       val stepStatusUrl = Bytes.toString(record.getValue(Bytes.toBytes("f"), Bytes.toBytes("getStepStatusUrl")))
       val json = parse(stepStatusUrl)
+      val finalStatus = (json \\ "status").children(0).extract[String]
+      /*
       val allStepDetails = (json \\ "stepDetails").children
       var flag = true
       var count = 0
@@ -50,7 +73,29 @@ object HBase_Read_Oracle_Insert {
       }
 
       val finalStatus = if(count == 0) "completed" else "failed"
-      println("Final Status :: " + finalStatus)
+      */
+      //println("Final Status :: " + finalStatus)
+      val insertQuery =
+      s"""
+         | INSERT INTO job_status (jobId, fail_msg, status, cfg, start_ts, end_ts, all_step_status, parent_col)
+         | VALUES ('$rowKey', '$failMsg', '$st', '$cfg', '$addTimeStamp', '$doneTimeStamp', '$finalStatus', null)
+         """.stripMargin
+      //println(sqlStatement)
+      stmt.execute(insertQuery)
+
+      /*
+      rowCount = rowCount + 1
+      if(rowCount % batchSize == 0) {
+        stmt.executeLargeBatch()
+        stmt.clearBatch()
+      }
+      */
+
+    }
+
+    con.commit()
+    if(con != null) {
+      con.close()
     }
   }
 }
